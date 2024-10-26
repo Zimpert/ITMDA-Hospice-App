99f2eafa057a634a36a4480aa2ed960a7b4f852a
Dylan-coder-arch <dylan.crocker8@gmail.com>
2024-10-26 14:46:40 +0200

    Stages

--- HospiceApp/MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ HospiceApp/MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bc25eaace2dc1488068f575d03beb775b1e810cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+72f5a53a3067c9e34d2d1a3762dc7fbcd9156cae")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
