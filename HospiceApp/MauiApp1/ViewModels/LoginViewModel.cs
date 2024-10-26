9df9576cfb55691d4727c6861b7ff5ab040ad8b3
Dylan-coder-arch <dylan.crocker8@gmail.com>
2024-10-26 14:55:21 +0200

    x

---
+++ HospiceApp/MauiApp1/ViewModels/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiApp1.Interfaces;
+
+
+namespace MauiApp1.ViewModels
+{
+    public partial class LoginViewModel :ObservableObject
+    {
+        private readonly IRequestManager _requestManager;
+        public LoginViewModel(IRequestManager requestManager)
+        {
+            _requestManager = requestManager;
+        }
+
+        [ObservableProperty]
+        string loginText;
+
+        [ObservableProperty]
+        string passwordText;
+
+        [RelayCommand]
+        public async Task Login()
+        {
+            var loginResult = await _requestManager.Login("/login", loginText, passwordText);
+            if (true)
+            {
+                Console.WriteLine("Login successful.");
+                App.Current.MainPage = new HomePage();
+                
+            }
+            else
+            {
+                Console.WriteLine("Login failed.");
+            }
+        }
+    }
+}
--- HospiceApp/MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ HospiceApp/MauiApp1/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+72f5a53a3067c9e34d2d1a3762dc7fbcd9156cae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+99f2eafa057a634a36a4480aa2ed960a7b4f852a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
