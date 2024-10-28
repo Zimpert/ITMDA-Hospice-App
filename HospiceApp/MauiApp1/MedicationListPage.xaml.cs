8facab01496cc5197e13abe6c2513c5f1b699ef8
David van Zyl <davidhvanzyl@gmail.com>
2024-10-28 11:36:06 -0700

    Added Shell navigation for homepage buttons Worked on basic navbar functionality Worked on login page

---
+++ HospiceApp/MauiApp1/MedicationListPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class MedicationListPage: ContentPage
+{
+	public MedicationListPage()
+	{
+		InitializeComponent();
+	}
+}
--- HospiceApp/MauiApp1/ProfilePage.xaml.cs
+++ HospiceApp/MauiApp1/ProfilePage.xaml.cs
@@ -18,5 +18,16 @@ namespace MauiApp1
                 vm.OnAppearing(); // Call the method on ViewModel
             }
         }
+
+        private async void OnBackButtonTapped(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("///HomePage"); // Navigate to the Homepage
+        }
+
+        private async void OnSettingsIconTapped(object sender, EventArgs e)
+        {
+            // Navigate to the Settings page 
+            await Shell.Current.GoToAsync("///SettingsPage"); // Navigate to the SettingsPage
+        }
     }
 }
