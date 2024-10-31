1c0e7d02dc2bf67fe24c69d2e9a8cc271863c469
Davidotk <davidhvanzyl@gmail.com>
2024-10-31 07:14:23 -0700

    -Finished navbar for all pages

--- HospiceApp/MauiApp1/Views/MedicationTrackerPage.xaml.cs
+++ HospiceApp/MauiApp1/Views/MedicationTrackerPage.xaml.cs
@@ -6,4 +6,14 @@ public partial class MedicationTrackerPage: ContentPage
 	{
 		InitializeComponent();
 	}
+	private async void OnBackButtonTapped(object sender, EventArgs e)
+{
+    await Shell.Current.GoToAsync("///HomePage"); // Navigate to the Homepage
+}
+
+private async void OnSettingsIconTapped(object sender, EventArgs e)
+{
+     
+    await Shell.Current.GoToAsync("///SettingsPage"); // Navigate to the SettingsPage
+}
 }
