f47b655f90b07509e152dcf7e29ab773394decfb
Dylan-coder-arch <119755439+Dylan-coder-arch@users.noreply.github.com>
2024-11-14 01:32:16 +0200

    Fix App 99% Done

--- HospiceApp/MauiApp1/Views/CaretakerQR.xaml.cs
+++ HospiceApp/MauiApp1/Views/CaretakerQR.xaml.cs
@@ -15,23 +15,15 @@ public partial class CaretakerQR : ContentPage
     }
 
 
-
-    private void OnSaveButtonClicked(object sender, EventArgs e)
+    private async void OnBackButtonTapped(object sender, EventArgs e)
     {
-        // Your code to handle the save action goes here.
-        // For now, you can display a simple alert to confirm the button click.
-        DisplayAlert("Save", "Save to Camera Roll button clicked.", "OK");
+        await Shell.Current.GoToAsync("///HomePage"); // Navigate to the Homepage
     }
 
-    private async void OnBackButtonTapped(object sender, EventArgs e)
-{
-    await Shell.Current.GoToAsync("///HomePage"); // Navigate to the Homepage
-}
-
-private async void OnSettingsIconTapped(object sender, EventArgs e)
-{
+    private async void OnSettingsIconTapped(object sender, EventArgs e)
+    {
      
-    await Shell.Current.GoToAsync("///SettingsPage"); // Navigate to the SettingsPage
-}
+        await Shell.Current.GoToAsync("///SettingsPage"); // Navigate to the SettingsPage
+    }
 
 }
--- HospiceApp/MauiApp1/Views/TaskPage.xaml.cs
+++ HospiceApp/MauiApp1/Views/TaskPage.xaml.cs
@@ -18,7 +18,7 @@ namespace MauiApp1.Views
 
         private async void OnBackButtonTapped(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("///HomePage"); // Navigate to the Homepage
+            await Shell.Current.GoToAsync("///ContactsPage"); // Navigate to the Homepage
         }
 
         private async void OnSettingsIconTapped(object sender, EventArgs e)
